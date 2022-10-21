18b70779e53f4f91495ae3d5d9ebd6f6220e0c63
isaacfavour58 <isaacfavour5858@gmail.com>
2022-10-21 17:28:02 +0100

    worked on the media query

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,43 @@
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+const navlinks = document.querySelector('.navlinks')
+const navlinksEl = document.querySelectorAll('.navlinks a')
+const navOpen = document.querySelector('.open')
+const navClose = document.querySelector('.close')
+
+function openNav(){
+  navlinks.style.top = "50%";
+  navOpen.style.display = "none"
+  navClose.style.display = "block"
+}
+
+
+function closeNav(){
+    navlinks.style.top = "-70%";
+    navOpen.style.display = "block"
+    navClose.style.display = "none"
+  }
+  
+  navlinksEl.forEach(navLink => {
+    navLink.addEventListener('click', function(){
+      closeNav()
+    })
+  })
